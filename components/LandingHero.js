c16acc33b1db668f2b98d234c3107b8ee0608d04
rum-n <rumen.mnv@protonmail.com>
2023-01-26 16:10:48 +0200

    small change + changing github credentials

--- components/LandingHero.js
+++ components/LandingHero.js
@@ -4,9 +4,7 @@ function LandingHero({}) {
   return (
     <div className="h-full bg-custom-gray pt-16 pb-40">
       <div className="absolute inset-x-0 mt-64 bg-white mx-auto w-9/12 flex flex-col items-center justify-center h-56 z-10 shadow-lg text-center">
-        <h3 className="text-gray-700 text-2xl font-medium">
-          VIOLETA BOYADZHIEVA
-        </h3>
+        <h3 className="text-2xl font-medium">VIOLETA BOYADZHIEVA</h3>
         <h4>
           Mixed Media Artist with love for surrealism, plants and peace. Welcome
           to my website!
